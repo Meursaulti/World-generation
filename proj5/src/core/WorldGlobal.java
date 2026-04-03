bb8ca9784326e4b7ce70e8508429854c64f84ff6
HuangXingXiang1 <3517561952@qq.com>
2026-04-03 19:54:57 +0800

    初稿

---
+++ proj5/src/core/WorldGlobal.java
@@ -0,0 +1,10 @@
+package core;
+
+import tileengine.TETile;
+
+import java.util.Random;
+
+public class WorldGlobal {
+	public static Random random =null;
+	public static TETile[][] world = null;
+}
